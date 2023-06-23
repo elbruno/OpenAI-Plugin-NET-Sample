3f41bb4ddba0d26faa6491825f1595fdb74b6dfd
El Bruno <bcapuano@gmail.com>
2023-06-23 14:27:26 -0400

    working sample for ChatGPT

--- src/OpenAI.PlugIn.NETSample/Program.cs
+++ src/OpenAI.PlugIn.NETSample/Program.cs
@@ -14,90 +14,49 @@ builder.Services.AddCors(options =>
     });
 });
 
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
+    options.EnableAnnotations();
     options.SwaggerDoc("v1", new OpenApiInfo
     {
-        Title = "ChatGPT.Weather",
+        Title = "El Bruno Pet Search",
         Version = "v1",
-        Description = "A plugin that allows you to get current weather information for a given city"
+        Description = "Search through El Bruno's wide range of pets."
     });
 });
 
 var app = builder.Build();
 app.UseCors("AllowAll");
-// Configure the HTTP request pipeline.
 
-app.UseSwagger(c =>
+// need to serialize this as V2 to work as ChatGPT API plugin
+app.UseSwagger(options =>
 {
-    c.PreSerializeFilters.Add((swaggerDoc, httpReq) =>
-    {
-        swaggerDoc.Servers = new List<OpenApiServer> { new OpenApiServer { Url = $"{httpReq.Scheme}://{httpReq.Host.Value}" } };
-    });
+    options.SerializeAsV2 = true;
 });
-app.UseSwaggerUI(x =>
+app.UseSwaggerUI(options =>
 {
-    x.SwaggerEndpoint("/swagger/v1/swagger.yaml", "ChatGPT.Weather v1");
-
+    options.SwaggerEndpoint("/swagger/v1/swagger.yaml", "ElBruno.Pets v1");
 });
 
-
-
+// necessary public files for ChatGPT to get plugin information
 app.UseStaticFiles(new StaticFileOptions
 {
     FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), ".well-known")),
     RequestPath = "/.well-known"
 });
 
-/// <summary>
-/// Gets the weather for a latitude and longitude
-/// </summary>
-app.MapGet("/weather", async (double latitude, double longitude) =>
+app.MapGet("/pets", () =>
 {
-    using (var httpClient = new HttpClient())
-    {
-        // 
-        return new GetWeatherResponse(10, 10, 10);
-    }
+    using var httpClient = new HttpClient();
+    var petsFile = File.ReadAllText("data/pets.json");
+    return Results.Json(petsFile);
 })
-.WithName("GetWeatherForecast")
+.WithName("Pets")
 .WithOpenApi(generatedOperation =>
 {
-    generatedOperation.Description = "Gets the current weather for a city by latitude and longitude";
-    var parameter = generatedOperation.Parameters[0];
-    parameter.Description = "The latitude of the city to retrieve the current weather";
-
-    parameter = generatedOperation.Parameters[1];
-    parameter.Description = "The longitude of the city to retrieve the current weather";
-
+    generatedOperation.Description = "Gets the list of pets available in El Bruno's Pet catalog.";
     return generatedOperation;
 });
 
-
-
-
-
 app.Run();
-
-
-internal class GetWeatherResponse
-{
-    public double temperature { get; set; }
-    public double windspeed { get; set; }
-    public double winddirection { get; set; }
-
-    public GetWeatherResponse() { }
-    public GetWeatherResponse(double temperature, double windspeed, double winddirection)
-    {
-        this.temperature = temperature;
-        this.windspeed = windspeed;
-        this.winddirection = winddirection;
-    }
-}
-
-
-
